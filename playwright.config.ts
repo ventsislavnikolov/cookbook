e607bb5afc74d940198e763878f4bb705897f161
Ventsislav Nikolov <ventsislavnikolovv@gmail.com>
2026-04-23 16:16:39 +0300

    test: fall back to .env when .env.local missing in playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from "@playwright/test"
 import { config } from "dotenv"
 
 config({ path: ".env.local" })
+config({ path: ".env" })
 
 export default defineConfig({
   testDir: "./tests/e2e",
